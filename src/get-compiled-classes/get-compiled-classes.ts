917cca6e69a4c63b65a2760eda1fb9d9fdfc94be
Volodymyr Yepishev <i.m.bwca@gmail.com>
2020-05-11 13:59:34 +0300

    fixed the issue with values becoming class names in enums

--- src/get-compiled-classes/get-compiled-classes.ts
+++ src/get-compiled-classes/get-compiled-classes.ts
@@ -1,7 +1,7 @@
 export function getCssClassesAsArray(css: string): string[] | undefined {
   try {
-    const classes: string[] = css.match(/\.[a-z\d\-_]+/gi) as string[];
-    return classes.reduce((a: string[], b) => (a.includes(b) ? a : [...a, b]), []);
+    const classes: string[] = css.match(/(\.[a-z\d\-_]+)/gi) as string[];
+    return classes.reduce((a: string[], b) => (a.includes(b) ? a : [...a, b]), []).filter((i) => !/\d+/.test(i));
   } catch (e) {
     return;
   }
